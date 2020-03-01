9a7a591480fc7a826ea041138c7b5c97637c2459
gomitti <g0m1mtt1@outlook.jp>
2020-03-01 13:42:30 +0000

    静的ファイル確認

--- src/server.ts
+++ src/server.ts
@@ -15,24 +15,24 @@ app.use('/public', express.static(__dirname + '/static'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const router: express.Router = express.Router();
-router.get('/api/list', (_:express.Request, res:express.Response) => {
-  res.send("");
-});
+// const router: express.Router = express.Router();
+// router.get('/api/list', (_:express.Request, res:express.Response) => {
+//   res.send("");
+// });
 
-router.get('/api/create', (req:express.Request, res:express.Response) => {
-  //exists param -apikey
-  req.params;
-  res.send("");
-});
-router.post('/api/update', (_:express.Request, res:express.Response) => {
-  res.send("")
-});
-router.post('/api/update', (_:express.Request, res:express.Response) => {
-  res.send("")
-});
+// router.get('/api/create', (req:express.Request, res:express.Response) => {
+//   //exists param -apikey
+//   req.params;
+//   res.send("");
+// });
+// router.post('/api/update', (_:express.Request, res:express.Response) => {
+//   res.send("")
+// });
+// router.post('/api/update', (_:express.Request, res:express.Response) => {
+//   res.send("")
+// });
 
-app.use(router);
+//app.use(router);
 
 app.listen(conf.PORT,()=>{ 
     console.log("start")
