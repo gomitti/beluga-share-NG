9263193955dc1097a87d17953631c5667f024fad
gomitti <g0m1mtt1@outlook.jp>
2020-03-02 13:30:14 +0000

    静的ファイル指定

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
-import express = require('express');
-import * as conf from './conf';
+import express = require('express')
+import path = require('path')
+import * as conf from './conf'
 
 
-const app: express.Express = express();
+const app: express.Express = express()
 
 app.use((_, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use('/public', express.static(__dirname + '/static'));
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+app.use('/public', express.static(path.join(__dirname, '../static')))
 
 // body-parserに基づいた着信リクエストの解析
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // const router: express.Router = express.Router();
 // router.get('/api/list', (_:express.Request, res:express.Response) => {
